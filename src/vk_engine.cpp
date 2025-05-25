81593f30d9cb4296b2aa164c8b651f7c84bb51c6
monapink <91607167+antfightclub@users.noreply.github.com>
2025-05-25 11:44:37 +0200

    Set up swapchain

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -100,8 +100,39 @@ void VulkanEngine::init_vulkan()
 
 }
 
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height) {
+    vkb::SwapchainBuilder swapchainBuilder{ _chosenGPU, _device, _surface };
+
+    _swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+        .set_desired_format(VkSurfaceFormatKHR{ .format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR })
+        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+        .set_desired_extent(width, height)
+        .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+        .build()
+        .value();
+
+    _swapchainExtent = vkbSwapchain.extent;
+    // Store swapchain and its related images
+    _swapchain = vkbSwapchain.swapchain;
+    _swapchainImages = vkbSwapchain.get_images().value();
+    _swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
 void VulkanEngine::init_swapchain()
 {
+    create_swapchain(_windowExtent.width, _windowExtent.height);
+}
+
+void VulkanEngine::destroy_swapchain()
+{
+    vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+    // destroy swapchain resources
+    for (int i = 0; i < _swapchainImageViews.size(); i++) {
+        vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
+    }
 
 }
 
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -23,6 +23,13 @@ public:
 	VkDevice _device;
 	VkSurfaceKHR _surface;
 
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector <VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
 	//initializes everything in the engine
 	void init();
 
@@ -40,4 +47,7 @@ private:
 	void init_swapchain();
 	void init_commands();
 	void init_sync_structures();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
