05399d1402c5c23459c04d07b1894544a66dcbd4
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-07 15:33:43 +0200

    stop using draw image as transfer dst

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -203,7 +203,7 @@ void VulkanEngine::init_swapchain()
 
     VkImageUsageFlags drawImageUsages{};
     drawImageUsages |= VK_IMAGE_USAGE_TRANSFER_SRC_BIT;
-    drawImageUsages |= VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+    //drawImageUsages |= VK_IMAGE_USAGE_TRANSFER_DST_BIT;
     drawImageUsages |= VK_IMAGE_USAGE_STORAGE_BIT;
     drawImageUsages |= VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
 
@@ -225,6 +225,7 @@ void VulkanEngine::init_swapchain()
     // Depth image
     _depthImage.imageFormat = VK_FORMAT_D32_SFLOAT;
     _depthImage.imageExtent = drawImageExtent;
+
     VkImageUsageFlags depthImageUsages{};
     depthImageUsages |= VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
 
