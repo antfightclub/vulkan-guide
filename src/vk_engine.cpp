fc16259b6a6d010c7f1c31f4e08bca78e375e8f8
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-07 13:49:54 +0200

    frustum culling

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -737,6 +737,45 @@ void VulkanEngine::cleanup()
     loadedEngine = nullptr;
 }
 
+bool is_visible(const RenderObject& obj, const glm::mat4& viewproj) {
+    std::array<glm::vec3, 8> corners{
+    glm::vec3 { 1, 1, 1 },
+    glm::vec3 { 1, 1, -1 },
+    glm::vec3 { 1, -1, 1 },
+    glm::vec3 { 1, -1, -1 },
+    glm::vec3 { -1, 1, 1 },
+    glm::vec3 { -1, 1, -1 },
+    glm::vec3 { -1, -1, 1 },
+    glm::vec3 { -1, -1, -1 },
+    };
+
+    glm::mat4 matrix = viewproj * obj.transform;
+
+    glm::vec3 min = { 1.5, 1.5, 1.5 };
+    glm::vec3 max = { -1.5, -1.5, -1.5 };
+
+    for (int c = 0; c < 8; c++) {
+        // Project each corner into clip space
+        glm::vec4 v = matrix * glm::vec4(obj.bounds.origin + (corners[c] * obj.bounds.extents), 1.f);
+
+        // Perspective correction
+        v.x = v.x / v.w;
+        v.y = v.y / v.w;
+        v.z = v.z / v.w;
+
+        min = glm::min(glm::vec3{ v.x, v.y, v.z }, min);
+        max = glm::max(glm::vec3{ v.x, v.y, v.z }, max);
+    }
+    
+    // Check whether the clip space box is within view
+    if (min.z > 1.f || max.z < 0.f || min.x > 1.f || max.x < -1.f || min.y > 1.f || max.y < -1.f) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
 void VulkanEngine::draw()
 {
     update_scene();
@@ -879,7 +918,9 @@ void VulkanEngine::draw_geometry(VkCommandBuffer cmd) {
     opaque_draws.reserve(mainDrawContext.OpaqueSurfaces.size());
 
     for (uint32_t i = 0; i < mainDrawContext.OpaqueSurfaces.size(); i++) {
-        opaque_draws.push_back(i);
+        if (is_visible(mainDrawContext.OpaqueSurfaces[i], sceneData.viewproj)) {
+            opaque_draws.push_back(i);
+        }
     }
 
     // Sort the opaque surfaces by material and mesh
@@ -1456,11 +1497,16 @@ void MeshNode::Draw(const glm::mat4& topMatrix, DrawContext& ctx) {
         def.firstIndex = s.startIndex;
         def.indexBuffer = mesh->meshBuffers.indexBuffer.buffer;
         def.material = &s.material->data;
-
+        def.bounds = s.bounds;
         def.transform = nodeMatrix;
         def.vertexBufferAddress = mesh->meshBuffers.vertexBufferAddress;
 
-        ctx.OpaqueSurfaces.push_back(def);
+        if (s.material->data.passType == MaterialPass::Transparent) {
+            ctx.TransparentSurfaces.push_back(def);
+        }
+        else {
+            ctx.OpaqueSurfaces.push_back(def);
+        }       
     }
     
     // Recurse down
