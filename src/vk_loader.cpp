d5db27b67915e21eab9e2b4840f2c012739fcca3
monapink <91607167+antfightclub@users.noreply.github.com>
2025-06-07 13:59:08 +0200

    mip them maps, yo!

--- src/vk_loader.cpp
+++ src/vk_loader.cpp
@@ -154,7 +154,7 @@ std::optional<AllocatedImage> load_image(VulkanEngine* engine, fastgltf::Asset& 
                     imagesize.height = height;
                     imagesize.depth = 1;
                     
-                    newImage = engine->create_image(data, imagesize, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_USAGE_SAMPLED_BIT,false);
+                    newImage = engine->create_image(data, imagesize, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_USAGE_SAMPLED_BIT,true);
                     
                     stbi_image_free(data);
                 }
@@ -166,7 +166,7 @@ std::optional<AllocatedImage> load_image(VulkanEngine* engine, fastgltf::Asset& 
                 imagesize.width = width;
                 imagesize.height = height;
                 imagesize.depth = 1;
-                newImage = engine->create_image(data, imagesize, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_USAGE_SAMPLED_BIT,false);
+                newImage = engine->create_image(data, imagesize, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_USAGE_SAMPLED_BIT,true);
                 stbi_image_free(data);
             }
         },
@@ -184,7 +184,7 @@ std::optional<AllocatedImage> load_image(VulkanEngine* engine, fastgltf::Asset& 
                 imagesize.width = width;
                 imagesize.height = height;
                 imagesize.depth = 1;
-                newImage = engine->create_image(data, imagesize, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_USAGE_SAMPLED_BIT,false);
+                newImage = engine->create_image(data, imagesize, VK_FORMAT_R8G8B8A8_UNORM, VK_IMAGE_USAGE_SAMPLED_BIT,true);
                 
                 stbi_image_free(data);
             }
